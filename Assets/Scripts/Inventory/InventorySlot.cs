a3ad16a4bff00f29468a09949e845db2f4c8fd56
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2024-06-09 17:08:27 +0200

    Inventory works

---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Scripting;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts
+{
+    public class InventorySlot : VisualElement
+    {
+        public Image Icon;
+        public string ItemGuid = "";
+
+        public InventorySlot()
+        {
+            //Create a new Image element and add it to the root
+            Icon = new Image();
+            Add(Icon);
+
+            //Add USS style properties to the elements
+            Icon.AddToClassList("slotIcon");
+            AddToClassList("slotContainer");
+        }
+        
+
+        /// <summary>
+        /// Sets the Icon and GUID properties
+        /// </summary>
+        /// <param name="item"></param>
+        public void HoldItem(ItemDetails item)
+        {
+            Icon.image = item.Icon.texture;
+            ItemGuid = item.GUID;
+        }
+
+        /// <summary>
+        /// Clears the Icon and GUID properties
+        /// </summary>
+        public void DropItem()
+        {
+            ItemGuid = "";
+            Icon.image = null;
+        }
+
+        #region UXML
+        [Preserve]
+        public new class UxmlFactory : UxmlFactory<InventorySlot, UxmlTraits> { }
+
+        [Preserve]
+        public new class UxmlTraits : VisualElement.UxmlTraits { }
+        #endregion
+
+    }
+}
--- Assets/Scripts/Managers/UIManagerControler.cs
+++ Assets/Scripts/Managers/UIManagerControler.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
-using UnityEngine.InputSystem.HID;
-using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 
 
@@ -13,14 +11,14 @@ namespace Managers
 {
     public class UIManagerControler : MonoBehaviour
     {
-    
-        public static UIManagerControler Instance {get; private set; }
+
+        public static UIManagerControler Instance { get; private set; }
         public UIDocument uiDocument;
 
         [SerializeField] private VisualTreeAsset standartOverlay;
         [SerializeField] private VisualTreeAsset inventory;
         [SerializeField] private VisualTreeAsset menu;
-        
+
         private void Awake()
         {
             if (Instance is not null)
@@ -28,13 +26,15 @@ namespace Managers
                 Destroy(gameObject);
                 return;
             }
+
             Instance = this;
             DontDestroyOnLoad(this);
             uiDocument = GetComponent<UIDocument>();
             GetInventoryRefrences();
             GetSettingsRefrences();
+            GetInInventoryCodeRefrences();
         }
-    
+
         private void OnDestroy()
         {
             if (Instance == this) Instance = null;
@@ -45,10 +45,10 @@ namespace Managers
             if (uiDocument.visualTreeAsset == check) return true;
             return false;
         }
-        
+
         #region Timerbar
-        
-        
+
+
 
         #endregion
 
@@ -56,7 +56,7 @@ namespace Managers
 
         private const string N_Inventorybutton = "Inventory";
         private Button inventoryButton;
-        
+
         //In inventory
         private const string N_InventoryInInventoryButton = "InventoryInInventory";
         private Button inventoryInInventoryButton;
@@ -83,22 +83,64 @@ namespace Managers
         {
             InventoryToggle();
         }
-        
-        
+
+
         public void InventoryToggle()
         {
             if (CheckCurrentUI(inventory))
             {
                 uiDocument.visualTreeAsset = standartOverlay;
                 GetInventoryRefrences();
+                GetSettingsRefrences();
             }
             else
             {
                 uiDocument.visualTreeAsset = inventory;
                 GetInInventoryRefrences();
+                GetSettingsRefrences();
             }
         }
 
+        #region InInventoryCode
+
+        public List<InventorySlot> InventoryItems = new List<InventorySlot>();
+        private VisualElement m_Root;
+        private VisualElement m_SlotContainer;
+        private static VisualElement m_GhostIcon;
+
+        private void GetInInventoryCodeRefrences()
+        {
+            m_Root = GetComponent<UIDocument>().rootVisualElement;
+            m_GhostIcon = m_Root.Query<VisualElement>("GhostIcon");
+            m_SlotContainer = m_Root.Q<VisualElement>("SlotContainer");
+            GameController.OnInventoryChanged += GameController_OnInventoryChanged;
+            for (int i = 0; i < 20; i++)
+            {
+                InventorySlot item = new InventorySlot();
+                InventoryItems.Add(item);
+                //m_SlotContainer.Add(item);
+            }
+        }
+
+        public void GameController_OnInventoryChanged(string[] itemGuid, InventoryChangeType change)
+            {
+                //Loop through each item and if it has been picked up, add it to the next empty slot
+                foreach (string item in itemGuid)
+                {
+                    if (change == InventoryChangeType.Pickup)
+                    {
+                        var emptySlot = InventoryItems.FirstOrDefault(x => x.ItemGuid.Equals(""));
+                    
+                        if (emptySlot != null)
+                        {
+                            emptySlot.HoldItem(GameController.GetItemByGuid(item));
+                        }
+                    }
+                }
+            }
+        
+
+    #endregion
         #endregion
 
         #region Settings
@@ -136,6 +178,7 @@ namespace Managers
             {
                 uiDocument.visualTreeAsset = standartOverlay;
                 GetSettingsRefrences();
+                GetInventoryRefrences();
             }
             else
             {
