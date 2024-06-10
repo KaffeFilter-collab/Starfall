054b768deb8caffd8643bf70d1e0a40ed8e6a301
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2024-06-10 09:28:21 +0200

    itemlerp

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class Items : MonoBehaviour
+{
+
+     private Vector3 currentpostion;
+
+     private void Awake()
+     {
+         currentpostion = transform.position;
+     }
+
+     private void OnMouseDown()
+    {
+        Debug.Log("yes");
+        StartCoroutine(GotoInventory());
+    }
+
+    IEnumerator GotoInventory()
+    {
+        while (transform.position != new Vector3(8.37f,-4.55f,-1))
+        {
+            transform.position = Vector3.LerpUnclamped(currentpostion,new Vector3(8.37f,-4.55f,-1),0.05f);;
+            currentpostion = transform.position;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
